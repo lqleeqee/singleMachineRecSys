89e185656d23d71d7536ec611be9ac6d972b10a9
lizujun <lqleeqee@gmail.com>
2017-07-06 16:10:20 +0800

    lzj

--- tools/time_dec.py
+++ tools/time_dec.py
@@ -10,9 +10,9 @@ from functools import wraps
 
 def fn_timer(function):  
     @wraps(function)  
-    def function_timer(*args, **kwargs):  
+    def function_timer(self,*args, **kwargs):  
         t0 = time.time()  
-        result = function(*args, **kwargs)  
+        result = function(self,*args, **kwargs)  
         t1 = time.time()  
         print ("Total time running %s: %s seconds" %  
                (function.func_name, str(t1-t0))  
